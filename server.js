eda5a624ee5d56e1c6c20382c0334cd197274180
EmersonPereiraOliveira <emersonhaw@gmail.com>
2018-08-05 15:08:02 -0300

    Respondendo requisições

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //
-// Servidor padrão simples
+// # SimpleServer
 //
-// Servidor usando Socket.IO, Express, e Async.
+// A simple chat server using Socket.IO, Express, and Async.
 //
 var http = require('http');
 var path = require('path');
@@ -9,20 +9,22 @@ var path = require('path');
 var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
-///
 //Criar
-//
 var bodyParser = require('body-parser');
 var request = require('request');
 
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
 var router = express();
 var server = http.createServer(router);
 var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
-//
 //Criar
-//
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
 
@@ -33,11 +35,13 @@ var sockets = [];
 //Alterações - início
 //
 
+
+
+
 //Endereço de callback
 //router.get('/webhook', function (req, res) {
 //  res.send('ok');
 //});
-
 
 //Endereço de callback
 router.get('/webhook', function (req, res) {
@@ -63,13 +67,81 @@ router.post('/webhook', function (req, res){
       //Percorrer mensagens, e apresentá-las no console
       entry.messaging.forEach(function (event) {
         if (event.message) {
-          console.log(event.message);
+          //console.log(event.message);
+          tratarMensagem(event);
         }
       })
     })
     res.sendStatus(200);
   }
+
 });
+
+function tratarMensagem (event) {
+   var senderId = event.sender.id;
+   var recipientId = event.recipient.id;
+   var timeOfMessage = event.timestamp;
+   var message = event.message;
+
+   console.log("Mensagem recebida pelo usuário %d pela página %d", senderId, recipientId)
+
+   var messageId = message.mid;
+   var messageText = message.text;
+   var files = message.attachments;
+
+   if (messageText) {
+     switch(messageText){
+        case 'oi':
+          sendTextMessage(senderId, 'Oi, tudo bem com você');
+          break;
+        case 'tchau':
+          sendTextMessage(senderId, 'Até mais...');
+          break;
+        default:
+          sendTextMessage(senderId, 'Não entendi o que você disse...');
+          break;
+      }
+    }else if (attachments){
+      //Tratar anexos
+      console.log("Enviaram anexos");
+   }
+}
+
+//Prepara o objeto no formato que o facebook espera
+function sendTextMessage (recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  }
+  //Envia via esta função
+  callSendApi(messageData);
+}
+
+
+//Função default do Facebook para responder a requisições (Enviar mensagem para o usuário)
+function callSendApi (messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: 'EAAEF25KV2QgBAKsMhsc3ue7wN6Ta4lF4ZCyxRD9Vf9oxnNe36RnPOvqhMK2cYfoaKGMnswTN6L0fHAY85SRxMgORY4ZAau1LjxfYSzdkBkCOtFTTASgg9ZCRBa40dUEuCAK3HIZACCGllZB21V9YV9PxflOZCOe8IjxD8o5qQewOjL23mSPP8B' },
+    method: 'POST',
+    json: messageData
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log("Enviada com sucesso!");
+      var recipientId = body.recipientId;
+      var messageId = body.messageId;
+
+    }else{
+      console.log("Não foi possível enviar!");
+      console.log(error);
+    }
+  })
+}
+
 
 //
 //Alterações - Fim
